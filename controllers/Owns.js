57e6161a57bb750c599f1c9b016a50392d75e6b0
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-07-06 13:57:25 +0700

    successfully finished the owns problemgit add .git add . now linked all relations together correctly

--- controllers/Owns.js
+++ controllers/Owns.js
@@ -3,47 +3,35 @@ const { sequelize, Sequelize } = require('../models');
 const Op = Sequelize.Op
 
 const createOwns = async (req,res) => {
-    // const {customerName,customerAge} = req.body
-    // const {accountValue} = req.body
-    // const {branchName,branchSize} = req.body
-    // const {managerName,managerExperience} = req.body
-    // const {ownsDay} = req.body
-
+    const {customerName,customerAge} = req.body
+    const {accountValue} = req.body
+    const {branchName,branchSize} = req.body
     const {managerName,managerExperience} = req.body
-    const { branchName, branchSize } = req.body
+    const {ownsDay} = req.body
+
     const newManager = await db.Manager.create({
         name:managerName,
         experience:managerExperience
     })
     const newBranch = await db.Branch.create({
-        name: branchName,
-        size: branchSize,
+        name:branchName,
+        size:branchSize,
         manager_id:newManager.id
     })
-    res.status(201).send(newBranch)
-    // const newManager = await db.Manager.create({
-    //     name:managerName,
-    //     experience:managerExperience
-    // })
-    // const newBranch = await db.Branch.create({
-    //     name:branchName,
-    //     size:branchSize,
-    //     manager_id:newManager.id
-    // })
-    // const newAccount = await db.Account.create({
-    //     value:accountValue,
-    //     branch_id:newBranch.id
-    // })
-    // const newCustomer = await db.Customer.create({
-    //     name:customerName,
-    //     age:customerAge
-    // })
-    // const newOwns = await db.Owns.create({
-    //     account_id:newAccount.id,
-    //     customer_id:newCustomer.id,
-    //     day:ownsDay
-    // })
-    // res.status(201).send(newManager)
+    const newAccount = await db.Account.create({
+        value:accountValue,
+        branch_id:newBranch.id
+    })
+    const newCustomer = await db.Customer.create({
+        name:customerName,
+        age:customerAge
+    })
+    const newOwns = await db.Owns.create({
+        account_id:newAccount.id,
+        customer_id:newCustomer.id,
+        day:ownsDay
+    })
+    res.status(201).send(newOwns)
 }
 
 const getOwns = async (req,res) => {
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const managerRoutes = require('./routes/Manager')
 const branchRoutes = require('./routes/Branch')
 const accountRoutes = require('./routes/Account')
 const customerRoutes = require('./routes/Customer')
+const ownRoutes = require('./routes/Owns')
 
 app.use(cors())
 app.use(express.json())
@@ -15,6 +16,7 @@ app.use('/managers',managerRoutes)
 app.use('/branches',branchRoutes)
 app.use('/accounts',accountRoutes)
 app.use('/customers',customerRoutes)
+app.use('/owns',ownRoutes)
 
 db.sequelize.sync().then(() => {
     app.listen(8000, () => console.log('Server is running at port 8000'))
